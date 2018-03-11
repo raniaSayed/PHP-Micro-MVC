9f1d6940d6a25d2e8323634a2e38f7e9dd660a8d
raniaSayed <raniasayed313@gmail.com>
2018-03-11 22:57:39 +0200

    define global variables

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+define("DB_NAME","lab1_php");
+define("DB_SERVER","127.0.0.1");
+define("DB_USERNAME","rania");
+define("DB_PASSWORD","rania2017");
+define("CWD",__DIR__);
+define("DS",DIRECTORY_SEPARATOR);
+define("PS",PATH_SEPARATOR);
+
+define("CONTROLLERS_DIR",CWD.DS."controllers");
+define("MODELS_DIR",CWD.DS."models");
+define("LIBS_DIR",CWD.DS."libs");
+define("VIEWS_DIR",CWD.DS."views");
+define("HOME_CONTROLLER","Users");
+define("SITE_URL","http://localhost/mvc/raniaMVC/");
