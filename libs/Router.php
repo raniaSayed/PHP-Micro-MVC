3a1f930b44a27014b35c5b78830baba4e4c571c0
raniaSayed <raniasayed313@gmail.com>
2018-04-21 09:49:36 +0200

    edit router

--- libs/Router.php
+++ libs/Router.php
@@ -5,7 +5,23 @@
 class Router
 {
 
-  public function run(){
+  public static function run(){
+    $controller = null;
+    if(isset($_GET["cntrl"]) && !empty($_GET["cntrl"])){
+
+      if(is_readable(CONTROLLERS_DIR.DS.$controller."Controller.php")){
+
+        //$controller_name = ucwords($_GET['cntrl']);
+        $controller_name  = $_GET["cntrl"];
+        $controller = new $controller_name();
+      }else{
+        // $home = HOME_CONTROLLER;
+        // $controller = new $home();
+        $controller = new HOME_CONTROLLER();
+      }
+      //check action
+
+    }
 
   }
 }
